f33795d7df65435e40ed56c4bb5397b3e2decc4f
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-03-21 12:10:21 +0530

    Add files via upload

---
+++ Course Outcome 1/matrix.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class matrix {
+
+	public static void main(String[] args)
+	{
+		int[][] a=new int[10][10];
+		int[][] b=new int[10][10];
+		int[][] res=new int[10][10];
+		int i,j;
+		Scanner read=new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int r = read.nextInt();
+		System.out.println("Enter the number of columns:");
+		int c = read.nextInt();
+		System.out.println("Enter the elements in 1st matrix\n");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				a[i][j]=read.nextInt();
+			}
+		}
+		System.out.println("-----1st matrix-----\n");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				System.out.print(a[i][j]+"\t");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("Enter the elements in 2nd matrix\n");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				b[i][j]=read.nextInt();
+			}
+		}
+		System.out.println("-----2nd matrix-----\n");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				System.out.print(b[i][j]+"\t");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("-----Resultant matrix-----\n");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+				System.out.print(((a[i][j])+(b[i][j]))+"\t");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
