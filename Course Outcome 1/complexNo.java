1aee2bc4391d9e673b438da2c7037ec49ed43efe
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-03-24 15:49:32 +0530

    Add files via upload

---
+++ Course Outcome 1/complexNo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class complexNo
+{
+	int r1,r2;
+	String i1,i2;
+	public void getData()
+	{
+		Scanner read = new Scanner(System.in);
+		System.out.println("Enter the real part of the Complex Number");
+		this.r1=read.nextInt();
+		read.nextLine();
+		System.out.println("Enter the imaginary part of the Complex Number");
+		this.i1=read.nextLine();
+		n=n+1;
+	}
+	public void printData()
+	{
+		System.out.println("The Complex Number:"+this.r1+"+"+this.i1);
+	}
+	public static void main(String[] args)
+	{
+		complexNo c1= new complexNo();
+		c1.getData();
+		c1.printData();
+		complexNo c2= new complexNo();
+		c2.getData();
+		c2.printData();
+	}
+
+}
