a7e87ba351972ba719302bd229dc6ef53c77bbb1
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-03-24 15:50:32 +0530

    Add files via upload

---
+++ Testing/rectangle.java
@@ -0,0 +1,14 @@
+package javaprgm;
+
+public class rectangle {
+
+	public rectangle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
