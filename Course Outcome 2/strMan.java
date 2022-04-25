6b652cf48a1af978b5bc11efff22e685a0216b18
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-04-25 11:35:48 +0530

    Update strMan.java

--- Course Outcome 2/strMan.java
+++ Course Outcome 2/strMan.java
@@ -3,12 +3,12 @@ public class strMan
 {
 	public static void main(String [] args)
   {
-		String s="Who are you ?";
+		String s="		Who are you ?		";
 		System.out.println("UpperCase : "+s.toUpperCase());
 		System.out.println("LowerCase : "+s.toLowerCase());
 		System.out.println("Trim : "+s.trim());
 		System.out.println("Replace : "+s.replace("are you","am I"));
 		System.out.println("Length : "+s.length());
-		System.out.println("Character at position : "+s.charAt(4));
+		System.out.println("Character at position : "+s.charAt(6));
 	}
 }
