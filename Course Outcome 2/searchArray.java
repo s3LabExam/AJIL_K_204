ea1e8dbaa3f726850b33bb8bbf9a2c334c079dc5
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-04-25 11:20:32 +0530

    Update searchArray.java

--- Course Outcome 2/searchArray.java
+++ Course Outcome 2/searchArray.java
@@ -1,9 +1,33 @@
-
-public class searchArray {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+package CO2;
+import java.util.*;
+public class searchArray
+{
+	public static void main(String [] args)
+	{
+		int[] a=new int[10];
+		int b=0,k=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter number of elements");
+		k=s.nextInt();
+		System.out.println("Enter the elements");
+		for (int i=0;i<k;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		System.out.println("Enter the number to search");
+		int x=s.nextInt();
+		for (int i=0;i<k;i++)
+		{
+			if(a[i]==x)
+			{
+				System.out.println("Number found");
+				b=1;
+				break;
+			}	
+		}
+		if(b==0)
+		{
+			System.out.println("Number not found");
+		}
 	}
-
 }
