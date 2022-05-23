eb4e07de9f8b3ddc3cfb9395ca0385cbe86db82d
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-05-23 12:14:37 +0530

    Update avgPosNum.java

--- Course Outcome/avgPosNum.java
+++ Course Outcome/avgPosNum.java
@@ -1,30 +1,44 @@
 package javaprgm;
 import java.util.Scanner;
+class  negativeException extends Exception
+{  
+    public  negativeException(int num)
+    {  
+        super(num);
+    }  
+}
 public class avgPosNum
 {
+	static void checkNum (int num) throws  negativeException
+	{   
+	   if(num < 0)
+	   {   
+	       throw new negativeException(num+" is a negative number.");    
+	   }  
+	}    
 	public static void main(String[] args)
 	{
 		Scanner read = new Scanner(System.in);
 		System.out.println("Enter the limit:\t");
-		int n,i,num, sum = 0;
+		int n,i,num,sum = 0;
 		n = read.nextInt();
-		
-		  
-		      for(i=0;i<n;i++)
-		      {
-		    	 System.out.println("Enter the positive number:\t");
-		    	 num = read.nextInt();
-		    	 try
-		  		 {
-		    		 if(num<0);
-		  		 }
-		    	 catch
-		    	 {
-					System.out.println(e);
+		for(i=0;i<n;i++)
+		{
+		   System.out.println("Enter the positive number:\t");
+		   num = read.nextInt();
+		   try
+		   {
+		     checkNum(num);
+		   }
+		   catch(negativeException ex)
+		   {
+		    		System.out.println("Caught the exception");  
+		            System.out.println("Exception occured: " + ex);  
 					i--;
 					num = read.nextInt();
 		    	 }
-		      }
-		      System.out.println("Average:"+(sum/n));
+		    	sum=sum+num;  
+		   }
+		  System.out.println("Average:"+(sum/n));
 	}
 }
