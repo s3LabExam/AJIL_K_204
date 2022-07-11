9c29b2c309448c829174e28cb52b0e9cdc2a5c37
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-07-11 21:19:47 +0530

    Create co4_pg14.java

---
+++ Course Outcome 4/co4_pg14.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class co4_pg14
+{
+	public static void main(String[] args)
+  {
+		Deque<Integer> dq=new LinkedList<>();
+		dq.add(1);
+		dq.add(2);
+		dq.addFirst(3);
+		dq.addLast(4);
+		dq.push(5);
+		dq.offer(6);
+		dq.offerFirst(7);
+		System.out.print("DEQUE:  "+dq+" ");
+		dq.removeFirst();
+		System.out.println("\nDEQUE after removing first element");
+		System.out.print(dq+" ");
+		dq.removeLast();
+		System.out.println("\nDEQUE after removing last element");
+		System.out.print(dq+" ");
+  }
+}
