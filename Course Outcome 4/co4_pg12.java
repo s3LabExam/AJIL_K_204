8814bcda321092421f2cdcbf960e73b50fbd07ed
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-07-11 21:11:12 +0530

    Create co4_pg12.java

---
+++ Course Outcome 4/co4_pg12.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class co4_pg12
+{
+  public static void main(String[] args)
+  {
+		Stack<String> st=new Stack<>();
+		Scanner sc=new Scanner(System.in);
+		String str;
+		System.out.println("Enter the number of items");
+		int n=sc.nextInt(); 
+		sc.nextLine();	
+		System.out.println("Enter the elements");
+		for(int i=0;i<n;i++)
+    {
+			str=sc.nextLine();
+			st.push(str);
+    }	
+		System.out.println(st);
+		System.out.println("Enter the position of element to be deleted");
+		int sp=sc.nextInt();
+		st.remove(sp);
+		System.out.println(st);
+  }
+}
