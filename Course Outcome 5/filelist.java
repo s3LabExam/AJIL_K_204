3ed78a58fe58ca72ad2b4651553cc930caa81eb8
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-07-12 00:25:31 +0530

    Create filelist.java

---
+++ Course Outcome 5/filelist.java
@@ -0,0 +1,37 @@
+package myproject;
+import java.io.File;
+import java.io.*;
+import java.util.*;
+public class filelist
+{
+ public static void main(String[] args)
+ {
+   File file = new File("C:\\Users\\AJIL\\Documents\\java\\cycle1 op");
+   String[] list = file.list();
+   for(String str : list)
+   {
+     System.out.println(str);
+   }
+  System.out.println("\nSEARCHING FOR FILENAMES STARTING WITH 's':\n");
+  FilenameFilter filter = new FilenameFilter() 
+  {
+    public boolean accept(File dir, String fname)
+    {
+      return fname.startsWith("s");
+    }
+  };
+  String[] search = file.list(filter);
+  if(search == null)
+  {
+    System.out.println("File does not exist. .");
+  }
+  else
+  {
+    for(int i=0; i<search.length;i++)
+    {
+      String fn = search[i];
+      System.out.println(fn);
+    }
+  }
+ }
+}
