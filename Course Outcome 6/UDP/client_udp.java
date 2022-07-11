19386f203182e8f11e5a5d3294f618aa1e7855fe
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-07-12 01:06:10 +0530

    Create client_udp.java

---
+++ Course Outcome 6/UDP/client_udp.java
@@ -0,0 +1,16 @@
+package myproject;
+import java.io.*;
+import java.net.*;
+public class client_udp
+{
+public static void main(String[] args) throws IOException
+{
+DatagramSocket client= new DatagramSocket();
+InetAddress add=InetAddress.getByName("localhost");
+String str ="Hello...Server";
+byte[] bufBytes = str.getBytes();
+DatagramPacket datagramPacket=new DatagramPacket(bufBytes,bufBytes.length,add,1234);
+client.send(datagramPacket);
+client.close();
+}
+}
