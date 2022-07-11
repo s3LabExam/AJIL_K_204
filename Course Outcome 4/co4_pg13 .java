b7f33e265805551c479f8b9b04a8cb36322665d3
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-07-11 21:15:37 +0530

    Create co4_pg13 .java

---
+++ Course Outcome 4/co4_pg13 .java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class co4_pg13
+{
+	public static void main(String[] args) 
+  {
+		int n;
+		String str;
+		PriorityQueue<String> pqueue=new PriorityQueue<>();
+		System.out.println("Total count");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter data");
+		for(int i=0;i<n;i++)
+    {
+			str=sc.nextLine();
+			pqueue.add(str);
+    }
+		System.out.println("Peek: "+pqueue.peek());
+		System.out.println("Queue");
+		Iterator<String> itr1=pqueue.iterator();  
+		while(itr1.hasNext())
+    {  
+		  System.out.println(itr1.next());
+    }
+		System.out.println("Polling: "+pqueue.poll());
+		System.out.println("After polling data in Queue");
+		Iterator<String> itr2=pqueue.iterator();  
+		while(itr2.hasNext())
+    {  
+		  System.out.println(itr2.next());
+    }
+  }
+}
